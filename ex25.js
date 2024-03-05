664308686e244ff9b8a800fb24d8462a684c7d61
Asma Shahid <asmashahid795@gmail.com>
2024-03-05 11:44:27 +0500

    Add files via upload

---
+++ ex25.js
@@ -0,0 +1,13 @@
+// Alien color
+// create a variable  called  alien_green
+var alien_color = "green";
+// write an if statement to test wether the alien_color is green. if it is, print the message the player just earned 5 points.
+if (alien_color === "green") {
+    console.log("player just earned 5 points !");
+}
+// write one version of this program that passes the if test another
+// fails.(the version that fails have no out )
+alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("player just earned 5 points !");
+}
---
+++ ex25.ts
@@ -0,0 +1,16 @@
+// Alien color
+
+// create a variable  called  alien_green
+let alien_color: string = "green";
+
+// write an if statement to test wether the alien_color is green. if it is, print the message the player just earned 5 points.
+if (alien_color === "green") {
+  console.log("player just earned 5 points !");
+}
+
+// write one version of this program that passes the if test another
+// fails.(the version that fails have no out )
+ alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("player just earned 5 points !");
+}
